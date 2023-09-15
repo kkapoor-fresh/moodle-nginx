8f38f9c8082f86fed3a8338cec58b0c29270a4bb
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2023-09-15 10:51:25 -0700

    Eliminate wait for successful deployment

--- openshift/scripts/deploy-template.sh
+++ openshift/scripts/deploy-template.sh
@@ -18,38 +18,38 @@ oc rollout latest dc/$PHP_DEPLOYMENT_NAME -n $DEPLOY_NAMESPACE
 oc rollout latest dc/$CRON_DEPLOYMENT_NAME -n $DEPLOY_NAMESPACE
 
 # Check PHP deployment rollout status every 10 seconds (max 10 minutes) until complete.
-ATTEMPTS=0
-ROLLOUT_STATUS_CMD="oc rollout status dc/$PHP_DEPLOYMENT_NAME -n $DEPLOY_NAMESPACE"
-until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
-  $ROLLOUT_STATUS_CMD
-  ATTEMPTS=$((attempts + 1))
-  sleep 10
-done
+# ATTEMPTS=0
+# ROLLOUT_STATUS_CMD="oc rollout status dc/$PHP_DEPLOYMENT_NAME -n $DEPLOY_NAMESPACE"
+# until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
+#   $ROLLOUT_STATUS_CMD
+#   ATTEMPTS=$((attempts + 1))
+#   sleep 10
+# done
 
 # Check CRON deployment rollout status every 10 seconds (max 10 minutes) until complete.
-ATTEMPTS=0
-ROLLOUT_STATUS_CMD="oc rollout status dc/$CRON_DEPLOYMENT_NAME -n $DEPLOY_NAMESPACE"
-until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
-  $ROLLOUT_STATUS_CMD
-  ATTEMPTS=$((attempts + 1))
-  sleep 10
-done
+# ATTEMPTS=0
+# ROLLOUT_STATUS_CMD="oc rollout status dc/$CRON_DEPLOYMENT_NAME -n $DEPLOY_NAMESPACE"
+# until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
+#   $ROLLOUT_STATUS_CMD
+#   ATTEMPTS=$((attempts + 1))
+#   sleep 10
+# done
 
 oc project $DEPLOY_NAMESPACE
-echo "Listing pods.."
+echo "Listing pods..."
 
-oc get pods|grep $PHP_DEPLOYMENT_NAME
-sleep 30
-oc get pods -l deploymentconfig=$PHP_DEPLOYMENT_NAME --field-selector=status.phase=Running -o name
-sleep 20
-podNames=$(oc get pods -l deploymentconfig=$PHP_DEPLOYMENT_NAME --field-selector=status.phase=Running -o name)
-pwd
+# oc get pods|grep $PHP_DEPLOYMENT_NAME
+# sleep 30
+# oc get pods -l deploymentconfig=$PHP_DEPLOYMENT_NAME --field-selector=status.phase=Running -o name
+# sleep 20
+# podNames=$(oc get pods -l deploymentconfig=$PHP_DEPLOYMENT_NAME --field-selector=status.phase=Running -o name)
+# pwd
 echo "deploy1=$PHP_DEPLOYMENT_NAME is deployed" >> $GITHUB_OUTPUT
 
-oc get pods|grep $CRON_DEPLOYMENT_NAME
-sleep 30
-oc get pods -l deploymentconfig=$CRON_DEPLOYMENT_NAME --field-selector=status.phase=Running -o name
-sleep 20
-podNames=$(oc get pods -l deploymentconfig=$CRON_DEPLOYMENT_NAME --field-selector=status.phase=Running -o name)
-pwd
+# oc get pods|grep $CRON_DEPLOYMENT_NAME
+# sleep 30
+# oc get pods -l deploymentconfig=$CRON_DEPLOYMENT_NAME --field-selector=status.phase=Running -o name
+# sleep 20
+# podNames=$(oc get pods -l deploymentconfig=$CRON_DEPLOYMENT_NAME --field-selector=status.phase=Running -o name)
+# pwd
 echo "deploy2=$CRON_DEPLOYMENT_NAME is deployed" >> $GITHUB_OUTPUT
