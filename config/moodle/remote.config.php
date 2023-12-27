f3b18614c47d730174e28dc8d31678f0b640806b
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2023-12-26 17:19:08 -0800

    Updated session_redis_host in Moodle config

--- config/moodle/remote.config.php
+++ config/moodle/remote.config.php
@@ -13,7 +13,7 @@ $CFG->moodleappdir    = '/var/www/html';
 $CFG->prefix    = '';
 $CFG->tool_generator_users_password = 'moodle-gen-PWd';
 
-$CFG->session_redis_host = 'moodle-redis-cluster';
+$CFG->session_redis_host = 'redis';
 $CFG->session_handler_class = '\core\session\redis';
 $CFG->session_redis_port = 6379; // Optional if TCP. For socket use -1
 $CFG->session_redis_database = 0; // Optional, default is db 0.
