1c7fcd917583c0e70e5cd2ce50e9c2874f4b6b29
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-01-04 14:41:33 -0800

    Adjust migration completion test script

--- openshift/scripts/test-migration-complete.sh
+++ openshift/scripts/test-migration-complete.sh
@@ -1,2 +1,13 @@
-echo "Testing if file copy is complete..."
-cat /var/www/html/config.php || true
+#!/bin/bash
+
+canary_line='Moodle frontpage.'
+file='/var/www/html/index.php'
+
+echo 'Waiting for file copy to complete...'
+
+until grep -q "${canary_line}" "${file}"
+do
+  sleep 5s
+done
+
+echo 'File copy complete.'
